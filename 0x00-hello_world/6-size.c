718b213796d60434473efa2980998516fabf026e
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-13 08:37:28 -0700

    Commit for tasks 1-6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
-char charType;
-int intType;
-long int longType;
-long long int longlongType;
-float floatType;
-printf("Size of char: %lu bytes\n", (unsigned long)sizeof(charType));
-printf("Size of int: %lu bytes\n", (unsigned long)sizeof(intType));
-printf("Size of long int: %lu bytes\n", (unsigned long)sizeof(longType));
-printf("Size of long long int: %lu bytes\n", (unsigned long)sizeof(longlongType));
-printf("Size of float: %lu byte\n", (unsigned long)sizeof(floatType));
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("Size of char: %lu bytes\n", (unsigned long)sizeof(a));
+printf("Size of int: %lu bytes\n", (unsigned long)sizeof(b));
+printf("Size of long int: %lu bytes\n", (unsigned long)sizeof(c));
+printf("Size of long long int: %lu bytes\n", (unsigned long)sizeof(d));
+printf("Size of float: %lu byte\n", (unsigned long)sizeof(e));
 return (0);
 }
