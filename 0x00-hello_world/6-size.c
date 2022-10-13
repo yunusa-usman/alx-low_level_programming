c1276b332d9a978f512db1b6065efdac41852d0e
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-13 07:00:36 -0700

    Task 1-6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-
 /**
- * main - Prints sizes 
- *
- * Return: Always 0 (Success)
- */
+* main - Prints sizes
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    int intType;
-    float floatType;
-    long int longType;
-    long long int longlongType;
-    char charType;
-
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of long int: %zu bytes\n", sizeof(longType));
-    printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
-    printf("Size of char: %zu byte\n", sizeof(charType));    
+int intType;
+float floatType;
+long int longType;
+long long int longlongType;
+char charType;
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of long int: %zu bytes\n", sizeof(longType));
+printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
+printf("Size of char: %zu byte\n", sizeof(charType));
 return (0);
 }
