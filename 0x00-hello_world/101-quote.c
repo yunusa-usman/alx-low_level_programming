cb66485875c2c2663ae13ec17e0cfec73cd058b1
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-13 09:13:25 -0700

    Task 101 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful");
+	fprintf(stderr, "\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
-
