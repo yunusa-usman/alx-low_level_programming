2bba123805bc87387482182a4f7b5b7582942517
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-13 07:50:22 -0700

    my commit for task 1-6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
+char charType;
 int intType;
-float floatType;
 long int longType;
 long long int longlongType;
-char charType;
+float floatType;
+printf("Size of char: %zu bytes\n", sizeof(charType));
 printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of long int: %zu bytes\n", sizeof(longType));
 printf("Size of long long int: %zu bytes\n", sizeof(longlongType));
-printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of float: %zu byte\n", sizeof(floatType));
 return (0);
 }
