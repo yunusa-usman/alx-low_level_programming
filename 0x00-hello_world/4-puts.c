4c5ca778ec28b0dfdc71c66c58825ed60f0e7b3b
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-13 06:35:46 -0700

    Commit for task 1-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle")
+
+return (0);
+}
