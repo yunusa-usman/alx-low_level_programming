32bb72c357d02d79dbc7e7ea009ec34d1f6d318f
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-15 01:02:12 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; c++)
+		putchar(alphabet);
+		putchar('\n');
+
+	return (0);
+}
