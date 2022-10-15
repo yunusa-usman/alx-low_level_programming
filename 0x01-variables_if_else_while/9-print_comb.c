376e62a39a94dac32050169bd0acc1f75e369fbb
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-15 09:24:50 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,13 @@ int main(void)
 {
 int a;
 for (a = 0; a <= 9; a++)
-	{
-	    putchar((a % 10) + '0');
-	if (a == '9')
+{
+	putchar((a % 10) + '0');
+	if (a == 9)
 		continue;
 	putchar(',');
-		putchar(' ');
-	}
-
-	putchar('\n');
-	return (0);
+	putchar(' ');
+}
+putchar('\n');
+return (0);
 }
