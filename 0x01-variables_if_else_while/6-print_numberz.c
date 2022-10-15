759fe2b064f6f49b602e4fa7cb735fa7f58d2889
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-15 05:44:46 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - Print all single digit numbers of base 10
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+	putchar(a);
+putchar('\n');
+return (0);
+}
