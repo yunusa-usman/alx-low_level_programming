3f956e226948867c768a6b8261ab9cce61c20315
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-19 03:07:15 -0700

    Task 5 commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - A function that prints a character sign
+ * @c: the character to be examined
+ * Return: 1 if n is greater than zero
+ * 0 if n is zero and -1 if n is less than zero
+*/
+int print_sign(int n)
+{ 
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return(0);
+	}
+	else 
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
