f8a5b918cf4f2960fff3d60cfaea5eede0078944
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-20 11:49:33 -0700

    Task 0-3

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "main.h"
 /**
 * main - Print positive_or_negative() function
@@ -10,21 +7,6 @@
 
 int main(void)
 {
-int i;
-srand(time(0));
-i = rand() - RAND_MAX / 2;
-if (i > 0)
-printf("%d is positive\n", i);
-else if (i == 0)
-printf("%d is zero\n", i);
-else
-printf("%d is negative\n", i);
-return (0);
-/**
-* main - Prints positive_or_negative() value
-* Return: 0
-*/
-
 int i;
 i = 0;
 positive_or_negative(i);
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,21 +10,22 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
-
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
+int largest;
+if (a > b && b > c)
+{
+largest = a;
 }
-
+else if (b > c && b > a)
+{
+largest = b;
+}
+else if (c > b)
+{
+largest = c;
+}
+else
+{
+largest = b;
+}
+return (largest);
+}
