2132aa8c491d16e7da71f8c124e7e3fcdd1cd14c
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-21 00:36:36 -0700

    Task 2 edit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,22 +10,23 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a > b && b > c)
-{
-largest = a;
-}
-else if (b > c && b > a)
-{
-largest = b;
-}
-else if (c > b)
-{
-largest = c;
-}
-else
-{
-largest = b;
-}
-return (largest);
+	int largest;
+
+	if (a > b && b > c)
+	{
+		largest = a;
+	}
+	else if (b > c && b > a)
+	{
+		largest = b;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		largest = b;
+	}
+	return (largest);
 }
