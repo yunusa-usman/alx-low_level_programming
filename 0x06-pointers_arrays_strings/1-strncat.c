66df061f004d8fda613847b34ba973399169816a
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-10-26 11:11:59 -0700

    Task 102 edit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,18 +9,18 @@
  *
  * Return: A pointer to the resulting string dest.
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
+
 {
-	int index = 0, src_len = 0;
-
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
-	return (dest);
+int i, co;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (co = 0; co < n; co++)
+{
+dest[i + co] = src[co];
+if (src[co] == '\n')
+co = n;
+}
+return (dest);
 }
