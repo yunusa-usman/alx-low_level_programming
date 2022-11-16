cf4203bfb4471f0d6c9a2e774534ef7ae58ab031
yunusa-usman <ibrahimyunusa245@gmail.com>
2022-11-16 06:24:18 -0800

    All Tasks

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - addition function
+ * @a: first integer value
+ * @b: second integer value
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction function
+ * @a: first integer value
+ * @b: second integer value
+ * Return: subtraction result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication function
+ * @a: first integer value
+ * @b: second integer value
+ * Return: multiplication result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
